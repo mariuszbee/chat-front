956b86632c3f2387575391cf39fd840a247764f1
mariuszbee <bemariuszdev@gmail.com>
2024-07-18 16:14:52 +0200

    setup MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import './App.css';
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container>
+        <h1>Hello world!</h1>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
