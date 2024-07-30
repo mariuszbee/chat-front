e90867cd111889bbf7fb755196cd2ae31bea11e4
mariuszbee <bemariuszdev@gmail.com>
2024-07-30 19:35:25 +0200

    aply useCreateUser hook to SignUp component

--- src/hooks/useCreateUser.ts
+++ src/hooks/useCreateUser.ts
@@ -2,15 +2,15 @@ import { gql, useMutation } from '@apollo/client';
 import { User } from '../models/User';
 
 interface CreateUserInput {
-  createUserData: {
+  createUserInput: {
     email: string;
     password: string;
   };
 }
 
 const CREATE_USER = gql`
-  mutation CreateUser($createUserData: CreateUserInput!) {
-    createUser(createUserData: $createUserData) {
+  mutation CreateUser($createUserInput: CreateUserInput!) {
+    createUser(createUserInput: $createUserInput) {
       _id
       email
     }
