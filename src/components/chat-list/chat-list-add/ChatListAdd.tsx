292cde0cfacf633831cafc5eb915dd7f91d63e0c
mariuszbee <bemariuszdev@gmail.com>
2024-08-04 12:26:36 +0200

    add chat to chat list modal component

--- src/components/chat-list/chat-list-add/ChatListAdd.tsx
+++ src/components/chat-list/chat-list-add/ChatListAdd.tsx
@@ -1,4 +1,20 @@
-import { Box, Modal, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormGroup,
+  IconButton,
+  InputBase,
+  Modal,
+  Paper,
+  Stack,
+  Switch,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { grey } from '@mui/material/colors';
+import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
 interface ChatListAddProps {
   isOpen: boolean;
@@ -6,6 +22,7 @@ interface ChatListAddProps {
 }
 
 export const ChatListAdd = ({ isOpen, handleClose }: ChatListAddProps) => {
+  const [isPrivate, setIsPrivate] = useState(true);
   return (
     <Modal open={isOpen} onClose={handleClose}>
       <Box
@@ -14,16 +31,59 @@ export const ChatListAdd = ({ isOpen, handleClose }: ChatListAddProps) => {
           top: '50%',
           left: '50%',
           transform: 'translate(-50%, -50%)',
+          bgcolor: grey[700],
           width: 400,
-          bgcolor: 'background.paper',
           border: '2px solid #000',
           boxShadow: 24,
           p: 4,
         }}
       >
-        <Typography variant="h6" component="h2">
-          Add Chat
-        </Typography>
+        <Stack spacing={2}>
+          <Typography variant="h6" component="h2">
+            Add Chat
+          </Typography>
+          <FormGroup>
+            <FormControlLabel
+              style={{
+                width: 0,
+              }}
+              control={
+                <Switch
+                  defaultChecked
+                  value={isPrivate}
+                  onChange={(event) => setIsPrivate(event.target.checked)}
+                />
+              }
+              label="Private"
+            />
+          </FormGroup>
+          {isPrivate ? (
+            <Paper
+              sx={{
+                p: '2px 4px',
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <InputBase
+                sx={{
+                  ml: 1,
+                  flex: 1,
+                  p: '10px',
+                }}
+                placeholder="Search users"
+              />
+              <IconButton sx={{ p: '10px' }}>
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          ) : (
+            <TextField label="Chat name" fullWidth />
+          )}
+          <Button variant="outlined" fullWidth>
+            Save
+          </Button>
+        </Stack>
       </Box>
     </Modal>
   );
