760f3d0dc13a92c07a43a4fddbb81d84780f2da3
mariuszbee <bemariuszdev@gmail.com>
2024-08-05 18:56:25 +0200

    update gql schema

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -14,6 +14,8 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
+  /** A date-time string at UTC, such as 2019-12-03T09:54:33Z, compliant with the date-time format. */
+  DateTime: { input: any; output: any; }
 };
 
 export type Chat = {
@@ -31,14 +33,28 @@ export type CreateChatInput = {
   userIds?: InputMaybe<Array<Scalars['String']['input']>>;
 };
 
+export type CreateMessageInput = {
+  chatId: Scalars['String']['input'];
+  content: Scalars['String']['input'];
+};
+
 export type CreateUserInput = {
   email: Scalars['String']['input'];
   password: Scalars['String']['input'];
 };
 
+export type Message = {
+  __typename?: 'Message';
+  _id: Scalars['ID']['output'];
+  content: Scalars['String']['output'];
+  createAt: Scalars['DateTime']['output'];
+  userId: Scalars['String']['output'];
+};
+
 export type Mutation = {
   __typename?: 'Mutation';
   createChat: Chat;
+  createMessage: Message;
   createUser: User;
   removeChat: Chat;
   removeUser: User;
@@ -49,6 +65,11 @@ export type Mutation = {
 
 export type MutationCreateChatArgs = {
   createChatInput: CreateChatInput;
+};
+
+
+export type MutationCreateMessageArgs = {
+  createMessageInput: CreateMessageInput;
 };
 
 
