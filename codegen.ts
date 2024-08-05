ca4bc84d395dcddffbcd6cf77707680587a7eb82
mariuszbee <bemariuszdev@gmail.com>
2024-08-05 10:08:15 +0200

    update useCreateChat method to update apollo cache to include new chat and add chat fragment

--- codegen.ts
+++ codegen.ts
@@ -8,6 +8,9 @@ const config: CodegenConfig = {
     'src/gql/': {
       preset: 'client',
       plugins: ['typescript'],
+      presetConfig: {
+        fragmentMasking: false,
+      },
     },
   },
   watch: true,
--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,6 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
+    "\n  fragment ChatFragment on Chat {\n    _id\n    userId\n    isPrivate\n    userIds\n    name\n  }\n": types.ChatFragmentFragmentDoc,
     "\n  mutation CreateChat($createChatInput: CreateChatInput!) {\n    createChat(createChatInput: $createChatInput) {\n      _id\n      userId\n      isPrivate\n      userIds\n      name\n    }\n  }\n": types.CreateChatDocument,
     "\n  mutation CreateUser($createUserInput: CreateUserInput!) {\n    createUser(createUserInput: $createUserInput) {\n      _id\n      email\n    }\n  }\n": types.CreateUserDocument,
     "\n  query Chats {\n    chats {\n      _id\n      userId\n      isPrivate\n      userIds\n      name\n    }\n  }\n": types.ChatsDocument,
@@ -33,6 +34,10 @@ const documents = {
  */
 export function graphql(source: string): unknown;
 
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(source: "\n  fragment ChatFragment on Chat {\n    _id\n    userId\n    isPrivate\n    userIds\n    name\n  }\n"): (typeof documents)["\n  fragment ChatFragment on Chat {\n    _id\n    userId\n    isPrivate\n    userIds\n    name\n  }\n"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
