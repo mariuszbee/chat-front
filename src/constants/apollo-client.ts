d789b9c6daca1705a0b8e078e2b04400d845cf6f
mariuszbee <bemariuszdev@gmail.com>
2024-08-06 18:07:58 +0200

    add splitLink to apollo client to use ws or http

--- src/constants/apollo-client.ts
+++ src/constants/apollo-client.ts
@@ -1,10 +1,11 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache, split } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { API_URL, WS_URL } from './urls';
 import { excludedRoutes } from './excluded-routes';
 import { onLogout } from '../utils/logout';
 import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const logOutLink = onError((error) => {
   if (
@@ -25,7 +26,18 @@ const wsLink = new GraphQLWsLink(
   })
 );
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: logOutLink.concat(httpLink),
+  link: logOutLink.concat(splitLink),
 });
