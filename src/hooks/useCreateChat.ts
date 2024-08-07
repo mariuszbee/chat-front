cfb34364b989a437a6707f57965bec5f056716b6
mariuszbee <bemariuszdev@gmail.com>
2024-08-07 12:07:32 +0200

    udate fragments according to the new schema

--- src/hooks/useCreateChat.ts
+++ src/hooks/useCreateChat.ts
@@ -1,15 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { graphql } from '../gql';
-import { ChatFragment } from '../fragments/chat.fragment';
 
 const createChatDocument = graphql(`
   mutation CreateChat($createChatInput: CreateChatInput!) {
     createChat(createChatInput: $createChatInput) {
-      _id
-      userId
-      isPrivate
-      userIds
-      name
+      ...ChatFragment
     }
   }
 `);
